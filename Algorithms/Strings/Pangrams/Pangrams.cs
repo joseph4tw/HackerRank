82434828069413c594c76b2320713c00ca08786d
Joseph <joseph.serido@gmail.com>
2016-06-21 07:48:36 -0400

    Pangrams submission

---
+++ Algorithms/Strings/Pangrams/Pangrams.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Solution {
+    static void Main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+        
+        var input = Console.ReadLine().ToLower();
+        
+        var minAscii = 97;
+        var maxAscii = 122;
+        
+        const int PangramCharacterLimit = 26;
+        var pangramCharacterCount = 0;
+        
+        var dictionary = new Dictionary<int, int>();
+        
+        // setup dictionary
+        for (var i = minAscii; i <= maxAscii; ++i)
+        {
+            dictionary.Add(i, 0);
+        }
+        
+        var asciiBytes = Encoding.ASCII.GetBytes(input);
+        
+        for (var i = 0; i < asciiBytes.Length; ++i)
+        {
+            if (dictionary.ContainsKey(asciiBytes[i]) && dictionary[asciiBytes[i]] == 0)
+            {
+                pangramCharacterCount++;
+                dictionary[asciiBytes[i]] = 1;
+            }
+            
+            if (pangramCharacterCount >= PangramCharacterLimit)
+            {
+                Console.WriteLine("pangram");
+                return;
+            }
+        }
+        
+        Console.WriteLine("not pangram");
+    }
+}
